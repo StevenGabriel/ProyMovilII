032466564588d1e3df8ec119426a18efd3608921
Steven Gabriel Claros Tapia <StevenClarosT@outlook.com>
2021-12-05 23:06:23 -0400

    Muchos Cambios

--- app/src/main/java/com/example/proymovilii/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/proymovilii/ui/home/HomeFragment.java
@@ -1,24 +1,25 @@
 package com.example.proymovilii.ui.home;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.proymovilii.R;
+import com.example.proymovilii.Visualizador_ChainSawMan;
 import com.example.proymovilii.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+    private ImageView ivChainsawMan, ivShinguekiNoKyojin;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,7 +29,21 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+        ivShinguekiNoKyojin = root.findViewById(R.id.ivShinguekiNoKyojin);
+        ivChainsawMan = root.findViewById(R.id.ivChainsawMan);
+
+        ivChainsawMan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), Visualizador_ChainSawMan.class);
+                startActivity(intent);            }
+        });
+
         return root;
+    }
+
+    public void Manga1(){
+
     }
 
     @Override
